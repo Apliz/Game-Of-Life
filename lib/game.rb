0a0b7fed3def34696828ff456294f5f90e15713f
Anton Plisnier <anttongp@gmail.com>
2020-07-30 12:24:26 +0200

    can check for neighbours accurately and step over to next generation with array display needs functional tests

--- lib/game.rb
+++ lib/game.rb
@@ -3,63 +3,103 @@ class Game
   attr_reader :grid, :condition
 
   def initialize
-    @grid = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] ,
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    @neighbors = 0
+    @current = [
+      [0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0]
     ]
+
+    @successor = [
+      [0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0]
+    ]
+   
   end
 
   def condition(choice)
     case choice
-      when 'glider'
-        @grid = [[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+      when 'loaf'
+        @current = [
+        [0, 0, 0, 0, 0, 0], 
+        [0, 0, 1, 1, 0, 0], 
+        [0, 1, 0, 0, 1, 0], 
+        [0, 0, 1, 0, 1, 0], 
+        [0, 0, 0, 1, 0, 0], 
+        [0, 0, 0, 0, 0, 0]
       ]
-      when 'small exploder'
-        @grid = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    
+      when 'toad'
+        @current = [
+        [0, 0, 0, 0, 0, 0], 
+        [0, 0, 0, 0, 0, 0], 
+        [0, 0, 1, 1, 1, 0], 
+        [0, 1, 1, 1, 0, 0], 
+        [0, 0, 0, 0, 0, 0], 
+        [0, 0, 0, 0, 0, 0]
       ]
         
     end
   end
 
-  def state_check() 
-    result = 0
-    @grid.each_with_index { |row, row_index|
-      if row_index > 0 && row_index < 10 
-        row.each_with_index { |cell, cell_index| 
-          if cell_index > 0 && cell_index < 10
-            # puts grid[row_index][cell_index]
+  def count_neighbours(grid)
+    neighbours = 0
+    successor = [
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0]
+    ]
+    grid.each_with_index { |row, y|
+      row.each_with_index { |cell, x|
+
+        # so it wont check edges and return nil
+        if (y < 1 || y > 4) || (x < 1 || x > 4 )
+          next
+        end
+
+        # ruleset for neighbour check
+        [[(grid[y - 1][x - 1])],
+        [(grid[y - 1][x])],
+        [(grid[y - 1][x + 1])],
+        [(grid[y][x - 1])],
+        [(grid[y][x + 1])],
+        [(grid[y + 1][x - 1])],
+        [(grid[y + 1][x])],
+        [(grid[y + 1][x + 1])]
+        ].each { |rule| 
+          p rule
+          #if neighbour exists, add one to total count
+          if rule == [1] 
+            neighbours += 1
           end
         }
-      end
+        # if there are 3 neighbours, or 2 adjacent to a live cell, create/ keep alive
+        if neighbours == 3 || (grid[y][x] == 1 && neighbours == 2)
+          successor[y][x] = 1
+          neighbours = 0
+        else
+          # else  destroy/ keep dead (possible optimisation)
+          successor[y][x] = 0
+          neighbours = 0
+        end
+
+      }
     }
+    p successor
   end
 
-end
+end 
+
+game = Game.new
+game.condition('loaf')
+game.state_check 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -9,23 +9,12 @@ describe Game do
 
   context 'loading conditions into game'
     it 'loads varying starting states into grid' do
-      expect{ subject.condition('glider') }.to change{ subject.grid[1][1] }.from(0).to(1)
+      expect{ subject.condition('loaf') }.to change{ subject.grid[1][2] }.from(0).to(1)
     end
   end
 
-  context 'update buffer' do
-    it 'updates grid to glider start configuration' do
-      subject = Game.new
-      subject.condition('glider')
-      p subject.state_check
-      expect(subject.buffer).not_to eq(subject.grid) 
-    end
-  end
-
-#   context 'detecting cell state' do
-#     it 'lists 0 actiations if none are present'
-#       expect(subject.conditions).to be_kind_of(Array)
-    
-#       expect(subject.state_check).to eq(0)
-#   end
-# end
+  # context 'neighbors' do
+  #   it 'initialises cell when 3 neighbors' do
+      
+  #   end
+  # end
