1c6d7967050e4df0d886d667e54c73439de444fb
Anton Plisnier <anttongp@gmail.com>
2020-07-30 18:18:33 +0200

    created board class for seperation of concern and dependency injection of check_edge() method into Game class; working on ruleset() method

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,11 @@
+require_relative 'board.rb'
+
 class Game
 
   attr_reader :grid, :condition
 
-  def initialize
+  def initialize(board = Board.new)
+    @board = board
     @neighbours = 0
     @current = []
     @successor = [
@@ -55,11 +58,6 @@ class Game
     end
   end
 
-  # don't check outermost array edges
-  def check_edge?(x, y)
-    true if (y < 1 || y > 6) || (x < 1 || x > 6 )
-  end
-
   # search neighbour cells
   def count_neighbours(x, y, grid = @current)
     [ [grid[y - 1][x - 1]],
@@ -82,7 +80,7 @@ class Game
   def start(grid = @current)
     grid.each_with_index { |row, y|
       row.each_with_index { |cell, x|
-        next if check_edge?(x, y) == true
+        next if @board.check_edge(x, y) == true
         count_neighbours(x, y)
         ruleset(x, y) ? @successor[y][x] = 1 : @successor[y][x] = 0
         @neighbours = 0
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,14 +1,5 @@
 require 'game'
 
 describe Game do
-  context 'grid has correct dimension' do
-    it 'should return correct array cell count' do
-      expect(subject.grid.length).to eq(6)
-    end
-  end
-
-  context 'loading conditions into game'
-    it 'loads varying starting states into grid' do
-      expect{ subject.condition('loaf') }.to change{ subject.grid[1][2] }.from(0).to(1)
-    end
-  end
+    
+end
