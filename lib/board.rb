53a8a6a758f51402e95ceba1553ce55146c276fb
Anton Plisnier <anttongp@gmail.com>
2020-07-30 20:28:04 +0200

    Board ruleset method dependency injection implemented

--- lib/board.rb
+++ lib/board.rb
@@ -3,18 +3,14 @@ class Board
   attr_reader :check_edge, :ruleset
 
   def initialize()
-
   end
 
-  # correctly injected
   def check_edge(x, y)
     true if (y < 1 || y > 6) || (x < 1 || x > 6 )
   end
 
-  # currently does not return valid array method (NoMethodError) 
-  # I think this is because of grid = @current where @current is not defined. (research/ thknk about solution.)
-  def ruleset(x, y, grid = @current)
-    @neighbours == 3 || (grid[y][x] == 1 && @neighbours == 2) ? true : false
+  def ruleset(x, y, grid, neighbours)
+    neighbours == 3 || (grid[y][x] == 1 && neighbours == 2) ? true : false
   end
 
 
--- lib/game.rb
+++ lib/game.rb
@@ -2,7 +2,7 @@ require_relative 'board.rb'
 
 class Game
 
-  attr_reader :grid, :condition
+  attr_reader :current, :condition
 
   def initialize(board = Board.new)
     @board = board
@@ -58,7 +58,6 @@ class Game
     end
   end
 
-  # search neighbour cells
   def count_neighbours(x, y, grid = @current)
     [ [grid[y - 1][x - 1]],
       [grid[y - 1][x]],
@@ -71,18 +70,12 @@ class Game
     ].each { |rule| @neighbours += 1 if rule == [1] }
   end
 
-  # conways rules
-  def ruleset(x, y, grid = @current)
-    @neighbours == 3 || (grid[y][x] == 1 && @neighbours == 2) ? true : false
-  end
-
-
   def start(grid = @current)
     grid.each_with_index { |row, y|
       row.each_with_index { |cell, x|
         next if @board.check_edge(x, y) == true
         count_neighbours(x, y)
-        ruleset(x, y) ? @successor[y][x] = 1 : @successor[y][x] = 0
+        @board.ruleset(x, y, grid, @neighbours) ? @successor[y][x] = 1 : @successor[y][x] = 0
         @neighbours = 0
       }
     }  
