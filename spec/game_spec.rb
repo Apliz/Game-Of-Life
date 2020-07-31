ab3576d77dfa6f942d3e6e8832e292f7944d389a
Anton Plisnier <anttongp@gmail.com>
2020-07-31 14:49:22 +0200

    additions to pattern_spec, Game.condition method is correctly tested and all tests passing

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,5 +1,70 @@
 require 'game'
 
 describe Game do
+
+        # testing this methods effect on Game has to come
+      # from the Game class spec (the receiver) and not pattern (the sender)
+      # at this stage I with current knowledge and time I can only test explicit output
+      # and then affirm integration with parallelisation of tests.
+  describe '.condition' do
+    context 'when loading loaf from Pattern' do   
+      it 'returns the loaf grid' do
+          fake_game = double
+          fake_pattern = double
+          allow(fake_pattern).to receive(:loaf).and_return([
+            [0, 0, 0, 0, 0, 0, 0, 0], 
+            [0, 0, 0, 0, 1, 1, 0, 0], 
+            [0, 0, 0, 1, 0, 0, 1, 0], 
+            [0, 0, 0, 0, 1, 0, 1, 0], 
+            [0, 0, 0, 0, 0, 1, 0, 0], 
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0]
+          ])
+          allow(fake_game).to receive(:condition).with('loaf').and_return(fake_pattern.loaf)
+  
+          expect(subject.condition('loaf')).to eq(fake_game.condition('loaf'))
+      end
+
+    end
     
+    context 'when loading toad from Pattern' do
+      it 'returns the toad grid' do
+        fake_game = double
+        fake_pattern = double
+        allow(fake_pattern).to receive(:toad).and_return([
+          [0, 0, 0, 0, 0, 0, 0, 0], 
+          [0, 0, 0, 0, 0, 0, 0, 0], 
+          [0, 0, 0, 0, 0, 0, 0, 0], 
+          [0, 0, 0, 1, 1, 1, 0, 0], 
+          [0, 0, 1, 1, 1, 0, 0, 0], 
+          [0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0]
+        ])
+        allow(fake_game).to receive(:condition).with('toad').and_return(fake_pattern.toad)
+
+        expect(subject.condition('toad')).to eq(fake_game.condition('toad'))
+      end
+    end
+
+    context 'when loading test from Pattern' do
+      it 'returns the test grid' do
+        fake_game = double
+        fake_pattern = double
+        allow(fake_pattern).to receive(:test).and_return([
+          [0, 0, 0, 0, 0, 0, 0, 0], 
+          [0, 0, 1, 1, 0, 0, 1, 0], 
+          [0, 1, 1, 1, 0, 0, 0, 0], 
+          [0, 0, 0, 0, 0, 0, 0, 0], 
+          [0, 0, 0, 0, 0, 0, 0, 0], 
+          [0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0]
+        ])
+        allow(fake_game).to receive(:condition).with('test').and_return(fake_pattern.test)
+        expect(subject.condition('test')).to eq(fake_game.condition('test'))
+      end
+    end
+  end
 end
--- spec/pattern_spec.rb
+++ spec/pattern_spec.rb
@@ -4,7 +4,7 @@ require 'game'
 describe Pattern do
   
   describe '.loaf' do
-    context 'when selecting start pattern' do
+    context 'when selecting loaf pattern' do
       it 'is a 2d array' do
         expect(subject.loaf).to be_a(Array)
       end
@@ -19,21 +19,41 @@ describe Pattern do
           [0, 0, 0, 0, 0, 0, 0, 0],
           [0, 0, 0, 0, 0, 0, 0, 0]
         ])
+      end      
+    end
+  end
+
+  describe '.toad' do
+    context 'when selecting toad pattern' do
+      it 'returns the toad starting grid' do
+        expect(subject.toad).to eq([
+          [0, 0, 0, 0, 0, 0, 0, 0], 
+          [0, 0, 0, 0, 0, 0, 0, 0], 
+          [0, 0, 0, 0, 0, 0, 0, 0], 
+          [0, 0, 0, 1, 1, 1, 0, 0], 
+          [0, 0, 1, 1, 1, 0, 0, 0], 
+          [0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0]
+        ])
       end
+    end
+  end
 
-      # testing this methods effect on Game has to come
-      # from the Game class spec (the receiver) and not pattern (the sender)
-      # at this stage I with current knowledge and time I can only test explicit output
-      # and then affirm integration with parallelisation of tests.
-      # let(:pattern) {Pattern.create}
-      # let(:loaf) {double('loaf')}
-      # # let(:pattern) {double([[0,0,0,0], [0,0,0,0]])}
-
-      # it 'responds to Game class' do
-      #   allow(game).to receive(:condition.and_return([[0,0,0,0], [0,0,0,0]])
-
-      #   expect(game.condition).to eq [[0,0,0,0], [0,0,0,0]]
-      
+  describe '.test' do
+    context 'when selecting test pattern' do
+      it 'returns the test starting grid' do
+        expect(subject.test).to eq([
+          [0, 0, 0, 0, 0, 0, 0, 0], 
+          [0, 0, 1, 1, 0, 0, 1, 0], 
+          [0, 1, 1, 1, 0, 0, 0, 0], 
+          [0, 0, 0, 0, 0, 0, 0, 0], 
+          [0, 0, 0, 0, 0, 0, 0, 0], 
+          [0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0]
+        ])
+      end
     end
   end
 end
